b7db237e46d374e92c56f7b5b05b028f1c541b84
Joshverde <52259302+Joshverde@users.noreply.github.com>
2025-01-31 16:32:57 -0500

    Task 2 - Modifying Customer Orders.

--- cc_3.js
+++ cc_3.js
@@ -4,3 +4,13 @@ let nums = [1,2,3,4,5];
 nums.push(6);
 nums.shift() ;
 console.log(nums)
+
+// Task #2
+//  Creates an array and changes the value of item 3 to 50.
+let qty = [10,45,15,25,35,40];
+qty[2]= qty[2] + 5;  //Java starts at 0
+// sums items in qty array
+total_qty = qty.reduce((sum,qty) => sum + qty,0 ); 
+console.log("quantities: ",qty);
+console.log("Total Quantity: ",total_qty)
+
